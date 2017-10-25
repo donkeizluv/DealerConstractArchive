7bc9a4b583c653be698fa8b0de3ebba077d77403
Luu Hong <donkeizluv@gmail.com>
2017-10-25 17:24:40 +0700

    print doc works

--- DealerContractArchive/wwwroot/js/Home/API.js
+++ DealerContractArchive/wwwroot/js/Home/API.js
@@ -2,5 +2,5 @@
 const AddContractApiUrl = "/API/ContractListing/AddNewContract"; //post
 const UploadContractApiUrl = "/API/ContractListing/UploadScan?dealerId={id}";
 const GetScanPdfApiUrl = "/Scan/GetScan?scanId={id}";
-const GetDocumentApiUrl = "/Document/GetDocument?contractid={id}&docName={name}";
+const GetDocumentApiUrl = "/Document/GetDocument?dealerId={id}&docName={name}";
 const CurrentHost = window.location.protocol + '//' + window.location.host;
